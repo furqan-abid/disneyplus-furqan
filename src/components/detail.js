f97ce529e2b4e4d9ffc2f65e0d922d313c1c6d5b
furqan-abid <furqanabid10@gmail.com>
2023-07-31 18:04:19 +0500

    details updated

--- src/components/detail.js
+++ src/components/detail.js
@@ -8,7 +8,7 @@ function Detail(props) {
         window.scrollTo(0,0)
     }, [])
     return (
-
+//this is detail component
         <Container>
             {
                 props.movies.map((movie)=>{
